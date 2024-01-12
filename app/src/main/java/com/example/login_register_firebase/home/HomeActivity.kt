7dc4bb8396b2a87083a3575a30ec5bf00a837d8f
SlavicDev1 <slavicisbased@gmail.com>
2024-01-12 17:53:42 +0100

    FINALLY FIXED THE R ERROR I WAS SO DUMB

--- app/src/main/java/com/example/login_register_firebase/home/HomeActivity.kt
+++ app/src/main/java/com/example/login_register_firebase/home/HomeActivity.kt
@@ -2,7 +2,7 @@ package com.example.loginregisterfirebase.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.loginregisterfirebase.R
+import com.example.login_register_firebase.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
